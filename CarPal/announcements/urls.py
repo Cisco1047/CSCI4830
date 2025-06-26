022441c076f1d2b9d730f90942fc88cab7988003
michaelisrael1 <165958703+michaelisrael1@users.noreply.github.com>
2025-06-25 23:11:58 -0500

    Delete CarPal directory

--- CarPal/announcements/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'announcements'
-
-urlpatterns = [
-    path('', views.announcements_list, name="list"),
-    path('<slug:slug>', views.announcement_page, name="page"),
-]
