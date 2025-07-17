4d36244799b025573bd5f3cae17c710c4af35830
michael <misrael740@gmail.com>
2025-07-16 22:56:51 -0500

    Added the backend for the database

--- MaintenanceApp/carPal/urls.py
+++ MaintenanceApp/carPal/urls.py
@@ -23,7 +23,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage),
-    path('', include('login.urls'))
+    path('', include('login.urls')),
+    path('vehicles/', include('selectVehicle.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
